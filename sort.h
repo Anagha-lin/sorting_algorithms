b85c3fd8845d6b8cf2e5c2828de9b443e6a46eaa
Anagha-lin <linusanaghamay12.com>
2023-12-19 05:44:53 -0800

    a

--- sort.h
+++ sort.h
@@ -30,7 +30,7 @@ void shell_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2);
 void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
